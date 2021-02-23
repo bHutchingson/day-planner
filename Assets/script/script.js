3f94fb3256c60adbcc8a12883936000d86dffe28
Brandon <hutchingsonb@gmail.com>
2021-02-23 12:13:59 -0500

    added local storage that saves each time frame

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -3,7 +3,7 @@ var currentDay = $('#currentDay');
 var currentTime = moment().startOf('hour').format('LT');
 var beforeTime = moment().startOf('day').add(0, 'hours');
 
-
+var resetTime = moment().startOf('day').add(0, 'hours');
 
 //adds timer and counts to the second
 var timeInterval = setInterval(function() {
@@ -56,6 +56,9 @@ $('#fourPm').text(fourPm);
 var time5 = $('#time5');
 var fivePm = beforeTime.add(1, 'hour').format('LT');
 $('#fivePm').text(fivePm);
+
+var midnight = resetTime.add(24, 'hour').format('LT');
+console.log(midnight);
 
 function timeBetween() {
    
@@ -145,5 +148,91 @@ function timeBetween() {
 
 };
 
-console.log(nineAm);
-console.log(currentTime);
+var input9 = $('#input9');
+var input10 = $('#input10');
+var input11 = $('#input11');
+var input12 = $('#input12');
+var input1 = $('#input1');
+var input2 = $('#input2');
+var input3 = $('#input3');
+var input4 = $('#input4');
+var input5 = $('#input5');
+
+
+// saves 9am to do
+$('#click9').click(function() {
+    input9 = $('#input9').val();
+    console.log(input9);
+    localStorage.setItem.JSON.Stringify("inputVal9", input9);
+});
+
+// saves 10am to do
+$('#click10').click(function() {
+    input10 = $('#input10').val();
+    console.log(input10);
+    localStorage.setItem("inputVal10", input10);
+});
+
+// saves 11am to do
+$('#click11').click(function() {
+    input11 = $('#input11').val();
+    console.log(input11);
+    localStorage.setItem("inputVal11", input11);
+});
+
+// saves 12am to do
+$('#click12').click(function() {
+    input12 = $('#input12').val();
+    console.log(input12);
+    localStorage.setItem("inputVal12", input12);
+});
+
+// saves 1am to do
+$('#click1').click(function() {
+    input1 = $('#input1').val();
+    console.log(input1);
+    localStorage.setItem("inputVal1", input1);
+});
+
+// saves 2am to do
+$('#click2').click(function() {
+    input2 = $('#input2').val();
+    console.log(input2);
+    localStorage.setItem("inputVal2", input2);
+});
+
+// saves 3am to do
+$('#click3').click(function() {
+    input3 = $('#input3').val();
+    console.log(input3);
+    localStorage.setItem("inputVal3", input3);
+});
+
+// saves 4am to do
+$('#click4').click(function() {
+    input4 = $('#input4').val();
+    console.log(input4);
+    localStorage.setItem("inputVal4", input4);
+});
+
+// saves 5am to do
+$('#click5').click(function() {
+    input5 = $('#input5').val();
+    console.log(input5);
+    localStorage.setItem("inputVal5", input5);
+});
+
+//reload local storage
+    input9.val(localStorage.getItem("inputVal9"));
+    input10.val(localStorage.getItem("inputVal10"));
+    input11.val(localStorage.getItem("inputVal11"));
+    input12.val(localStorage.getItem("inputVal12"));
+    input1.val(localStorage.getItem("inputVal1"));
+    input2.val(localStorage.getItem("inputVal2"));
+    input3.val(localStorage.getItem("inputVal3"));
+    input4.val(localStorage.getItem("inputVal4"));
+    input5.val(localStorage.getItem("inputVal5"));
+
+
+
+
